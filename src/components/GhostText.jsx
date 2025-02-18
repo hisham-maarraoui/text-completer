5c08029cc035db61bfbdc71641c49ce131da8214
sohumgautam17 <sohumgautam@icloud.com>
2025-02-17 23:15:01 -0500

    Start text-comppleter chrome extension

---
+++ src/components/GhostText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const GhostText = ({ text, position }) => {
+    return (
+        <div
+            className="text-gray-400">
+
+                
+            {text}
+        </div>
+    )
+}
